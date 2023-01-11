23c54ca42d196e9a9d37787467cde30ef229667f
baldgenie <siddhartha.kapoor@gmail.com>
2023-01-12 01:26:16 +0545

    commit

--- pages/profile.js
+++ pages/profile.js
@@ -24,14 +24,14 @@ const Profile = () => {
 
     return (
 
-        <div className="px-[24px] lg:px-[50px] py-[40px] ">
+        <div className="px-[24px] lg:px-[50px] py-[30px] ">
 
             <div className="flex space-x-3 lg:space-x-5 2xl:space-x-7">
-                <img src={profile_Dp} alt="" className="h-[80px] xl:h-[100px]" />
+                <img src={profile_Dp} alt="" className="h-[90px] xl:h-[100px]" />
 
                 <div className="pt-5 flex flex-col space-y-[2px]">
-                    <div className="flex space-x-1">
-                        <h1 className="font-inter font-semibold text-textblack text-[12px] lg:text-[16px]">{name}</h1>
+                    <div className="flex space-x-1 items-center">
+                        <h1 className="font-inter font-semibold text-textblack text-[14px] lg:text-[16px]">{name}</h1>
                         {verified &&
                             <img src="/profile/bluetick.svg" alt="" className="h-[14px] lg:h-[17px]" />
                         }
@@ -41,12 +41,12 @@ const Profile = () => {
 
                         {memeCategories.map((category, index) => {
                             return (
-                                <h2 key={category} className="font-inter  text-[#414249] text-[10px] lg:text-[14px]">{index === memeCategories.length - 1 ? category : `${category},`}</h2>
+                                <h2 key={category} className="font-inter  text-[#414249] text-[12px] lg:text-[14px]">{index === memeCategories.length - 1 ? category : `${category},`}</h2>
                             )
                         })}
                     </div>
 
-                    <h2 onClick={followClick} className="select-none gradientBlue font-inter font-semibold  text-[11px] lg:text-[15px] cursor-pointer">{followed ? "Unfollow" : "Follow"}</h2>
+                    <h2 onClick={followClick} className="select-none gradientBlue font-inter font-semibold  text-[15px] lg:text-[18px] cursor-pointer">{followed ? "Unfollow" : "Follow"}</h2>
                 </div>
             </div>
 
